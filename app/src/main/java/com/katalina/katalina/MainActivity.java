b150784714c094177f1c0b55f3ef94543c98e358
Beohrn <Enigma1>
2016-04-28 13:50:38 +0300

    commit bla-bla-bla

--- app/src/main/java/com/katalina/katalina/MainActivity.java
+++ app/src/main/java/com/katalina/katalina/MainActivity.java
@@ -36,71 +36,85 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 //        LeakCanary.install(getApplication());
 
-        final NotificationManager nm = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification.Builder builder = new Notification.Builder(getApplicationContext())
-                .setSmallIcon(R.drawable.ic_launcher)
-                .setTicker("RecApp")
-                .setAutoCancel(true)
-                .setContentIntent(null);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            // build a complex notification, with buttons and such
-
-            RemoteViews notificationView = new RemoteViews(
-                    getApplicationContext().getPackageName(),
-                    R.layout.notification_layout
-            );
-
-            notificationView.setImageViewResource(
-                    R.id.imagenotileft,
-                    R.drawable.ic_launcher);
-            final Intent intent = new Intent(getApplicationContext(), VoiceRecognitionService.class);
-
-            if (!isSerivceUp) {
-                intent.setAction("STOP");
-                notificationView.setOnClickPendingIntent(R.id.stop, unInitialize(getApplicationContext(), intent));
-//                notificationView.setTextViewText(R.id.start, "START");
-            } else {
-                notificationView.setOnClickPendingIntent(R.id.start, initialize(getApplicationContext(), intent));
-//                notificationView.setTextViewText(R.id.start, "Start");
-            }
-
-//            findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    startService(intent);
-//                }
-//            });
+//        final NotificationManager nm = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+//        Notification.Builder builder = new Notification.Builder(getApplicationContext())
+//                .setSmallIcon(R.drawable.ic_launcher)
+//                .setTicker("RecApp")
+//                .setAutoCancel(true)
+//                .setContentIntent(null);
 //
-//            findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    stopService(intent);
-//                }
-//            });
-            notificationView.setTextViewText(R.id.title, getTitle());
-            notificationView.setTextViewText(R.id.text, "Tap on Start");
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+//            // build a complex notification, with buttons and such
+//
+//            RemoteViews notificationView = new RemoteViews(
+//                    getApplicationContext().getPackageName(),
+//                    R.layout.notification_layout
+//            );
+//
+//            notificationView.setImageViewResource(
+//                    R.id.imagenotileft,
+//                    R.drawable.ic_launcher);
+//            final Intent intent = new Intent(getApplicationContext(), VoiceRecognitionService.class);
+//
+//            if (!isSerivceUp) {
+//                intent.setAction("STOP");
+//                notificationView.setOnClickPendingIntent(R.id.stop, unInitialize(getApplicationContext(), intent));
+////                notificationView.setTextViewText(R.id.start, "START");
+//            } else {
+//                notificationView.setOnClickPendingIntent(R.id.start, initialize(getApplicationContext(), intent));
+////                notificationView.setTextViewText(R.id.start, "Start");
+//            }
+//
+////            findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+////                @Override
+////                public void onClick(View v) {
+////                    startService(intent);
+////                }
+////            });
+////
+////            findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+////                @Override
+////                public void onClick(View v) {
+////                    stopService(intent);
+////                }
+////            });
+//            notificationView.setTextViewText(R.id.title, getTitle());
+//            notificationView.setTextViewText(R.id.text, "Tap on Start");
+//
+//
+//            builder = builder.setContent(notificationView);
+//
+//        } else {
+//            // Build a simpler notification, without buttons
+//            builder = builder.setContentTitle(getTitle())
+//                    .setContentText("LOLOLOLO")
+//                    .setSmallIcon(android.R.drawable.ic_menu_gallery);
+//        }
+//
+//        final Notification notification = builder.build();
+//
+//        nm.notify(1010, notification);
+//
+//        Intent service = new Intent(this, VoiceRecognitionService.class);
+//        startService(service);
+//        finish();
+        final Intent intent = new Intent(this, VoiceRecognitionService.class);
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+            }
+        });
 
-
-            builder = builder.setContent(notificationView);
-
-        } else {
-            // Build a simpler notification, without buttons
-            builder = builder.setContentTitle(getTitle())
-                    .setContentText("LOLOLOLO")
-                    .setSmallIcon(android.R.drawable.ic_menu_gallery);
-        }
-
-        final Notification notification = builder.build();
-
-        nm.notify(1010, notification);
-
-        Intent service = new Intent(this, VoiceRecognitionService.class);
-        startService(service);
-        finish();
+        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+            }
+        });
 
     }
 
